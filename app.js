60534cde105e011786a1fb513180972c8d5ee65d
Chad Stabler <stablerpsn@gmail.com>
2022-05-04 14:06:06 -0500

    worked on displayFriends but after a bug finished displayMushrooms

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const mushroomsEl = document.querySelector('.mushrooms');
 const addMushroomButton = document.getElementById('add-mushroom-button');
 const addFriendButton = document.getElementById('add-friend-button');
 // initialize state
-
 let mushroomCount = 3;
 
 const friendData = [
@@ -41,20 +40,34 @@ addMushroomButton.addEventListener('click', () => {
 });
 
 addFriendButton.addEventListener('click', () => {
+    let friendName = friendInputEl.value;
     // get the name from the input
+    let friend = { name: friendName, satisfaction: 1, };
     // create a new friend object
+    friendData.push(friend);
     // push it into the friends state array, passed in as an argument
+    friendInputEl.value = '';
     // reset the input
+    displayFriends();
     // display all the friends (use a function here)
 });
 
 function displayFriends() {
     // clear out the friends in DOM
-
+    friendsEl.textContent = '';
     // for each friend in state . . .
     for (let friend of friendData) {
         // use renderFriend to make a friendEl
-
+        renderFriend(friend);
+        friendsEl.addEventListener('click', () => {
+            if (friend.satisfaction < 3 && mushroomCount > 0) {
+                friend.satisfaction++;
+                mushroomCount--;
+                displayFriends();
+                displayMushrooms();
+            }
+        });
+        friendsEl.append(friend);
         // this is a clickable list, so . . .
         //     add an event listener to each friend
         //         and if the friend's satisfaction level is below 3 and you have mushrooms left
@@ -67,8 +80,9 @@ function displayFriends() {
 
 function displayMushrooms() {
     // clear out the mushroom div
-
+    mushroomsEl.textContent = '';
     for (let i = 0; i < mushroomCount; i++) {
+        mushroomsEl.append(renderMushroom());
         // for each mushroom in your mushroom state, render and append a mushroom
     }
 }
